b742eda0e1695caa3e2aff964c269ac3a47a17c3
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2023-02-05 13:01:38 +0200

    klppokcsww3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,74 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+
 import Section from '../components/Section/Section';
 import FeedbackOptions from '../components/FeedbackOptions/FeedbackOptions';
 import Statistics from '../components/Statistics/Statistics';
 import Notification from '../components/Notification/Notification';
 
+const options = ['good', 'bad', 'neutral'];
+
+const App = () =>{
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const onLeaveFeedback = type => {
+    setFeedback(prevState => {
+      const value = prevState[feedback];
+      return { ...prevState, [feedback]: value + 1 };
+    });
+  };
+
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = feedback;
+    return good + bad + neutral;
+    };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return Math.round((feedback.good / total) * 100);
+  };
+  const { good, neutral, bad } = feedback;
+
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {this.countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </>
+  );
+}
+export default App;
+
+
+
+
+
+
+
+
+
+/*
 export class App extends Component {
   state = {
     good: 0,
@@ -58,5 +123,4 @@ export class App extends Component {
     );
   }
 }
-
-
+*/
